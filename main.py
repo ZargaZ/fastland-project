1cb39f724c2e6ba03f7a66226ba111118c6b365c
ZargaZ <zargazxgames@gmail.com>
2025-11-05 21:26:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI
 import psycopg2
 
-DATABASE_URL = "postgresql://postgres:Wasd3knopkI@db.wenkfujkfqfuatgtqsmo.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.wenkfujkfqfuatgtqsmo:Wasd3knopkI@aws-1-eu-west-1.pooler.supabase.com:6543/postgres"
 
 app = FastAPI()
 
@@ -30,5 +30,6 @@ def get_clients():
             return [{"id": r[0], "name": r[1], "contact": r[2], "phone": r[3]} for r in rows]
     else:
         return {"error": "DB not connected"}
+
 
 
